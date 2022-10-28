58c336ce231651635ffc13dfd5fee5eaa9ea7070
David B <bbddvv1611@gmail.com>
2022-10-28 12:48:11 +0200

    LED class implemented

---
+++ src/lichterkette/LED.java
@@ -0,0 +1,13 @@
+package lichterkette;
+
+public class LED {
+    public boolean leuchtet;
+
+    public boolean isLeuchtet() {
+        return leuchtet;
+    }
+
+    public void setLeuchtet(boolean leuchtet) {
+        this.leuchtet = leuchtet;
+    }
+}
