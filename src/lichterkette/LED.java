cab6c3e92504380ab5b1cd713167fc7e74ba7c5a
Verodoxen <timon.s@gmx.de>
2022-10-28 18:38:17 +0200

    Rgb added + getter and setter

--- src/lichterkette/LED.java
+++ src/lichterkette/LED.java
@@ -2,12 +2,24 @@ package lichterkette;
 
 public class LED {
     private boolean leuchtet;
+    private int RGB;
+    public LED()
+    {
 
+    }
     public boolean isLeuchtet() {
         return leuchtet;
     }
 
     public void setLeuchtet(boolean leuchtet) {
         this.leuchtet = leuchtet;
+    }
+    public void setFarbe(int pRGB)
+    {
+        this.RGB = pRGB;
+    }
+    public int getFarbe()
+    {
+        return RGB;
     }
 }
