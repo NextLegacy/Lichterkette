97ae8dbd5bc26baf4986f325a15f0a49f40acfd9
NextLegacy <mcserezli@gmail.com>
2022-10-31 10:07:17 +0100

    Tests for Window

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -2,4 +2,15 @@ package testing;
 
 public class Test 
 {
+    private Test() { }
+
+    public static void main(String[] args)
+    {
+        testGUI();
+    }
+
+    public static void testGUI()
+    {
+        
+    }
 }
