9711966837ebcc9ba5cfdb6c9e907a2f8812e2a8
NextLegacy <mcserezli@gmail.com>
2022-10-31 10:05:33 +0100

    Refactored

--- src/lichterkette/LED.java
+++ src/lichterkette/LED.java
@@ -1,25 +1,23 @@
 package lichterkette;
 
-public class LED {
-    private boolean leuchtet;
-    private int RGB;
-    public LED()
-    {
+public class LED 
+{
+    public static final int DEFAULT_COLOR = 0x000000;
 
-    }
-    public boolean isLeuchtet() {
-        return leuchtet;
+    private boolean active; // Active could be equivalent to 0x000000
+    private int rgb;
+    
+    public LED() { this(false, DEFAULT_COLOR); }
+    
+    public LED(boolean active, int rgb)
+    {
+        setActive(active);
+        setRGB(rgb);
     }
 
-    public void setLeuchtet(boolean leuchtet) {
-        this.leuchtet = leuchtet;
-    }
-    public void setFarbe(int pRGB)
-    {
-        this.RGB = pRGB;
-    }
-    public int getFarbe()
-    {
-        return RGB;
-    }
+    public boolean active() { return active; }
+    public int rgb() { return rgb; }
+
+    public void setActive(boolean state) { active = state; }
+    public void setRGB(int rgb) { this.rgb = rgb; }
 }
