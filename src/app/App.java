6bb4dc8f0026eb345f073442dadf3f54aba5835a
NextLegacy <mcserezli@gmail.com>
2022-11-11 00:50:20 +0100

    Created Main file for Project

---
+++ src/app/App.java
@@ -0,0 +1,51 @@
+package app;
+
+import engine.Engine;
+import engine.math.Vector;
+import engine.utils.graphics.Image;
+import lichterkette.LichterKette2D;
+
+import static engine.utils.MathUtils.*;
+
+import java.awt.event.KeyEvent;
+
+import app.scenes.StartScene;
+import app.scripts.LichterKettenScript;
+
+public class App 
+{
+    private App() { }
+
+    public static Engine engine;
+
+    public static void main(String[] args)
+    {
+        engine = new Engine(vec(1080, 720), 60, 60, "main");
+
+        engine.setActiveScene(new StartScene());
+
+        engine.activate();
+    }
+
+    public static void update(LichterKettenScript script, LichterKette2D lichterKetten)
+    {
+        Image image = Image.fromFile("./rsc/images/start.png");
+        
+        image = image.scaled(script.size);
+
+        System.out.println(script.size);
+
+        for (int x = 0; x < lichterKetten.getSize(); x++)
+        {
+            for (int y = 0; y < lichterKetten.getLichterkette(x).getSize(); y++)
+            {
+                lichterKetten.getLichterkette(x).get(y).setActive(true);
+                lichterKetten.getLichterkette(x).get(y).setRGB(image.colorBuffer()[image.getIndex(x, y)]);
+            }   
+        
+            //lichterKetten.getLichterkette(x).setActiveInGaps(1, false);
+        }
+
+        script.setSize(script.size.plus(engine.input().wheel().direction()));
+    }
+}
