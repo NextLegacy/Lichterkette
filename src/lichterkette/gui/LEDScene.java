b8002f2c4c057822971d5b558d3a4322639c2d0a
Verodoxen <timon.s@gmx.de>
2022-11-10 19:21:45 +0100

    scene create

--- src/lichterkette/gui/LEDScene.java
+++ src/lichterkette/gui/LEDScene.java
@@ -1,12 +1,31 @@
 package lichterkette.gui;
 
+import engine.GameObject;
 import engine.Scene;
+import engine.Script;
+import engine.math.Vector;
+import engine.utils.graphics.Image;
 
 public class LEDScene extends Scene 
 {
     @Override
     protected void init() 
     {
-        
+        GameObject gameObject = new GameObject();
+
+        addGameObject(gameObject);
+        gameObject.addScript(new Script() 
+        { 
+            @Override
+           protected void awake() {
+               setActive(true);
+           } 
+
+           @Override
+               protected void render() {
+                   image().drawImage(Image.fromFile("./rsc/menu.png"));
+               }
+        });
+        gameObject.setActive(true);
     }    
 }
